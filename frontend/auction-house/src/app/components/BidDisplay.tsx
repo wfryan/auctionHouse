fdc07db6662cbf67b6942838cc4bbee10c955ffa
echo532 <84986609+echo532@users.noreply.github.com>
2024-11-26 17:39:14 -0500

    Revert "added new frontend files"
    This reverts commit 74465d8ba4c61850ff73bbb57513d990805b9917.

--- frontend/auction-house/src/app/components/BidDisplay.tsx
+++ frontend/auction-house/src/app/components/BidDisplay.tsx
@@ -1,17 +1,11 @@
 import { Bid } from "../auction_page/page";
 
 export default function BidDisplay({bid}: {bid: Bid}){
-    const formatBidTime = (time: string) => {
-        const tmp = time.split(/[T.]/)
-        return `${tmp[0]} ${tmp[1]}`
-    }
-    const formattedTime = formatBidTime(bid.bidTime.toString())
-
     return(
         <div>
             <p>User: {bid.username}</p>
             <p>Amount: {bid.amount}</p>
-            <p>Time: {formattedTime}</p>
+            <p>Time: {bid.bidTime.toString()}</p>
         </div>
     )
 }
