28e7265d75cd51f8aeb092c7d23ff255bd213a83
echo532 <84986609+echo532@users.noreply.github.com>
2024-12-12 01:09:17 -0500

    fixed frontend search/auction page view

--- frontend/auction-house/src/app/components/AuctionItemClickable.tsx
+++ frontend/auction-house/src/app/components/AuctionItemClickable.tsx
@@ -2,10 +2,28 @@ import { AuctionItem } from "../search/page"
 
 export default function AuctionItemClickable({ aucItem }: { aucItem: AuctionItem }) {
     return (
-        <div>
-            <p className="text-black">item id: {aucItem.auction_item_id}</p>
-            <p className="text-black">item name: {aucItem.item_name}</p>
-            <p className="text-black">item info: {aucItem.information}</p>
+        <div className="h-full flex gap-8 bg-gray-100">
+            <div>
+                {aucItem.status == 'active' &&
+                    <h1 className="text-green-700 ">status: {aucItem.status}</h1>
+                }
+                {(aucItem.status == 'bought' || aucItem.status == 'archived') &&
+                    <h1 className="text-red-700 "> status: {aucItem.status}</h1>
+                }
+
+                <p className="text-black">item name: {aucItem.item_name}</p>
+                <p className="text-black">current price: ${aucItem.amount}</p>
+                <p className="text-black ">item info: {aucItem.information}</p>
+            </div>
+            {aucItem.picture && (
+                <div className="ml-auto">
+                    <img
+                        src={aucItem.picture}
+                        alt="Preview"
+                        className="w-32 h-32 object-contain mt-2"
+                    />
+                </div>
+            )}
         </div>
     )
 }
--- frontend/auction-house/src/app/search/page.tsx
+++ frontend/auction-house/src/app/search/page.tsx
@@ -6,11 +6,16 @@ import BuyerInfo from "../components/BuyerInfo"
 import SignOutButton from "../components/SignoutButton"
 import { getToken } from "../utils/cookie"
 import LoginButton from "../components/LoginButton"
+import { instance } from '../utils/auctionHouseApi';
 
 export interface AuctionItem {
     auction_item_id: number
     item_name: string
+    status: string
     information: string
+    amount: number
+    end_time: string
+    picture?: string
 }
 
 export default function Search() {
@@ -20,6 +25,8 @@ export default function Search() {
     const router = useRouter()
     const [auctions, setAuctions] = useState<AuctionItem[]>([])
     const [dispError, setDispError] = useState(false)
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
     const input = useRef<HTMLInputElement>(null)
 
 
@@ -27,11 +34,10 @@ export default function Search() {
         setIsMounted(true);
     }, []);
 
-    if (!isMounted) {
-        return null;
-    }
+
 
     const searchFunc = async () => {
+        setIsButtonDisabled(true);
         try {
             if (input.current == null) {
                 throw (new Error("error"))
@@ -40,16 +46,8 @@ export default function Search() {
                 search: input.current.value
             }
 
-            const resp = await fetch("https://9cf5it1p4d.execute-api.us-east-2.amazonaws.com/auctionHouse/items/searchItems", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(body)
-            })
-            const jsonResp = await resp.json()
-            console.log(jsonResp)
-            setAuctions(jsonResp.body.items)
+            const response = await instance.post('/items/searchItems', JSON.stringify(body))
+            setAuctions(response.data.body.items)
         }
         catch (error) {
             setDispError(true)
@@ -57,23 +55,64 @@ export default function Search() {
         }
     }
 
+    useEffect(() => {
+        // Set a timeout to re-enable the button after 2 seconds
+        if (isButtonDisabled) {
+            const timer = setTimeout(() => {
+                setIsButtonDisabled(false);
+            }, 2000); // 2000 milliseconds = 2 seconds
+
+            // Cleanup the timer on component unmount or when isButtonDisabled changes
+            return () => clearTimeout(timer);
+        }
+    }, [isButtonDisabled]);
+
+
+    useEffect(() => {
+        window.addEventListener("keydown", (e) => {
+            if (e.key === 'Enter' && !isButtonDisabled) {
+                searchFunc()
+            }
+        })
+    })
+
+    if (!isMounted) {
+        return null;
+    }
+
+
     const setStorage = async (itemId: number) => {
         localStorage.setItem("id", itemId.toString())
         router.push("/auction_page")
     }
 
+    function displayItem(item: AuctionItem): boolean {
+
+        if (item.status == 'active') {
+            return true;
+        }
+
+        if (getToken() != null) {
+            if (item.status == 'bought' || item.status == 'archived' /*further time checks required */) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+    }
 
     return (
         <div className="min-h-screen py-8 px-4">
             {/* Conditionally render BuyerInfo, LoginButton, and SignOutButton */}
-            {isMounted && getToken() != null && <BuyerInfo />}
-            <div className="mt-4">
-                {isMounted && <LoginButton />}
-                {isMounted && <SignOutButton />}
-            </div>
+            <div>{isMounted && getToken() != null && <BuyerInfo />}</div>
+            <div>{isMounted && <LoginButton />}</div>
+            <div>{isMounted && <SignOutButton />}</div>
 
             {/* Search bar section */}
-            <div className="mt-6 flex flex-col sm:flex-row items-center gap-4">
+            <div className="mt-6 px-4 flex flex-col sm:flex-row items-center gap-4">
                 <input
                     placeholder="Search for auction items..."
                     ref={input}
@@ -81,7 +120,7 @@ export default function Search() {
                     className="px-4 py-2 rounded-md border border-gray-300 focus:ring-2 text-black focus:ring-blue-500 focus:outline-none w-full sm:w-80"
                 />
                 <button
-                    onClick={() => searchFunc()}
+                    onClick={() => searchFunc()} disabled={isButtonDisabled}
                     className="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:outline-none"
                 >
                     Search Items
@@ -89,15 +128,28 @@ export default function Search() {
             </div>
 
             {/* Auctions section */}
-            <div className="mt-6 grid grid-cols-1 gap-6">
-                {auctions.map((item) => (
-                    <div key={item.auction_item_id} className="p-4 bg-white rounded-lg shadow-md w-30 hover:shadow-xl transition-shadow duration-300">
-                        <div onClick={() => setStorage(item.auction_item_id)}>
-                            <AuctionItemClickable aucItem={item} />
+            <div className="mt-6 px-5">
+                <div className="max-w-md">
+                    {auctions.length > 0 &&
+                        // modify to include bought if you are signed in
+                        auctions.filter(item => (displayItem(item)))
+                            .map((item) => (
+                                <div key={item.auction_item_id} className="mb-6 bg-gray-100 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
+                                    <div className="p-4" onClick={() => setStorage(item.auction_item_id)}>
+                                        <AuctionItemClickable aucItem={item} />
+                                    </div>
+                                </div>
+                            ))}
+                    {auctions.length == 0 && (
+                        <div>
+                            <label>No auctions found</label>
                         </div>
-                    </div>
-                ))}
+
+                    )}
+
+                </div>
             </div>
+
 
             {/* Error message */}
             {dispError && (
